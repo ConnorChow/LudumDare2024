e7b0d14cf94a6c86b1f7a7c89c4810a4de9db7ef
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-04-13 13:40:34 -0400

    recall

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,13 +10,15 @@ var curHealth : float
 
 @export var damage : float = 8
 
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var sprite : Sprite2D = $mainSprite
+@onready var recall_sprite = $recallSprite
 @onready var hitBox = $UiNodes/hitBox
 @onready var hitColor_timer = $UiNodes/hitBox/hitTimer
 @onready var grab_box = $grabBox
 @onready var hold_point = $holdPoint
 @onready var money = $UiNodes/Money
 @onready var grab_timer = $grabTimer
+@onready var recall_timer = $recallTimer
 
 
 var input_direction
@@ -24,7 +26,8 @@ var heldObject : Object = null
 
 var canBuy : bool = false
 var canGrab : bool = true
-
+var recalling : bool = false
+var homePosition : Vector2
 
 func _ready():
 	curHealth = maxHealth
@@ -41,6 +44,13 @@ func _physics_process(delta):
 	velocity.x = clamp(velocity.x, -modMaxSpeed, modMaxSpeed)
 	velocity = lerp(velocity, Vector2(0,0), delta*5)
 	velocity.normalized()
+	
+	#cancel recall
+	if recalling:
+		if !input_direction == Vector2(0,0) :
+			stopRecall()
+
+
 	if velocity.x > 0:
 		if !sprite.flip_h == false:
 				sprite.flip_h = false
@@ -54,6 +64,13 @@ func _physics_process(delta):
 func get_input():
 	input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity += input_direction * modSpeed
+	
+	if Input.is_action_just_pressed("recall"):
+		if heldObject != null:
+				heldObject.reparent(get_parent())
+				heldObject = null
+				modMaxSpeed = maxSpeed
+				modSpeed = speed
 	
 	if Input.is_action_just_pressed("grab"):
 		if canGrab:
@@ -75,7 +92,31 @@ func get_input():
 						modMaxSpeed = maxSpeed * .6
 						break
 
+#start recalling
+#press b, change sprite / animation / whatever
+#after x seconds, recall and change sprite back
+#can be cancelled early by moving, change sprite back
 
+func recall(homePos :Vector2):
+	if !recalling:
+		homePosition = homePos
+		sprite.visible=false
+		recall_sprite.visible = true
+		recalling = true
+		recall_timer.start()
+
+func stopRecall():
+	recalling = false
+	sprite.visible=true
+	recall_sprite.visible = false
+	recall_timer.stop()
+	
+func _on_recall_timer_timeout():
+	global_position = homePosition
+	takeDamage(6)
+	stopRecall()
+	pass # Replace with function body.
+	
 func updateCur():
 	money.set_text(str(CurrencyCount.currency)) 
 	heldObject = null
@@ -100,3 +141,6 @@ func _on_hit_timer_timeout():
 
 func _on_grab_timer_timeout():
 	canGrab = true
+
+
+
