75f17751c941b6cdaacaf12553bf1d892a0bfc91
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-04-13 00:08:30 -0400

    tweaks to player controller

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 10.0
-const MaxSpeed = 60
-var collects : int = 0
-var hasSeen : bool = false
-var canEnd : bool = false
-var score : int = 0
+@export var speed = 10.0
+@export var maxSpeed = 60
+@export var maxHealth = 30
+var curHealth
+
+@export var damage = 8
 
 @onready var sprite = $Sprite2D
 # Get the gravity from the pr$Sprite2Doject settings to be synced with RigidBody nodes.
@@ -16,8 +16,8 @@ var score : int = 0
 func _physics_process(delta):
 	
 	get_input()
-	velocity.y = clamp(velocity.y, -MaxSpeed, MaxSpeed)
-	velocity.x = clamp(velocity.x, -MaxSpeed, MaxSpeed)
+	velocity.y = clamp(velocity.y, -maxSpeed, maxSpeed)
+	velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
 	velocity = lerp(velocity, Vector2(0,0), delta*5)
 	velocity.normalized()
 	if velocity.x > 0:
@@ -32,4 +32,4 @@ func _physics_process(delta):
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity += input_direction * SPEED
+	velocity += input_direction * speed
