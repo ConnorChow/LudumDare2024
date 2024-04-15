334d7a3927f9666b8b5139fbab9eb5a0a182d676
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-04-15 17:29:44 -0400

    hid battle obejct

--- Player/Player.gd
+++ Player/Player.gd
@@ -149,7 +149,8 @@ func get_input():
 			if Input.is_action_just_pressed("1"):
 				placeObject("Light")
 			if Input.is_action_just_pressed("2"):
-				placeObject("Battle")
+				#placeObject("Battle")
+				pass
 			if Input.is_action_just_pressed("3"):
 				pass
 			if Input.is_action_just_pressed("4"):
