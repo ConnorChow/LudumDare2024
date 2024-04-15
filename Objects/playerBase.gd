3613d41c300837e532b8e364ca763394189b8f32
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-04-15 15:03:17 -0400

    "difficulty settings"

--- Objects/playerBase.gd
+++ Objects/playerBase.gd
@@ -4,6 +4,19 @@ var player
 
 @export var minion : PackedScene
 
+var difficulty 
+var currency
+
+func _ready():
+	difficulty = OptionsController.difficulty
+	match difficulty:
+		1:
+			currency = 3
+		2:
+			currency = 2
+		3:
+			currency = 1
+
 func _on_body_entered(body):
 	body.buyEnable() 
 	player = body
@@ -28,7 +41,7 @@ func _input(_event):
 
 func _on_currency_detector_area_entered(area):
 	if !area.is_in_group("Grab"): return
-	CurrencyCount.currency += 1
+	CurrencyCount.currency += currency
 	player.updateCur()
 	player.dropItem()
 	area.queue_free()
