08379bc9d5286b6957152d68f21b341d5a1a8f18
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2024-04-14 20:54:17 -0400

    music

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -2,12 +2,15 @@ extends Node
 
 @onready var optionsMenu = $Control/PanelContainer2
 @onready var streamer_mode = $Control/PanelContainer2/VBoxContainer/StreamerMode
+@onready var path = $Path2D/PathFollow2D
+
 
 func _ready():
 	pass # Replace with function body.
 
 
-
+func _physics_process(delta):
+	path.progress += 10*delta
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/World.tscn")
@@ -24,6 +27,6 @@ func _on_streamer_mode_pressed():
 
 
 
-func _on_difficulty_slider_drag_ended(value_changed):
+func _on_difficulty_slider_drag_ended(_value_changed):
 	OptionsController.difficulty = $Control/PanelContainer2/VBoxContainer/DifficultySlider.value
 
